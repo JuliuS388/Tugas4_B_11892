fe02c2e662a30d9708a318024c4549338a40a7c6
Julius Aditya <220711892@students.uajy.ac.id>
2024-11-06 15:34:21 +0700

    Update Tugas_Modul4_B_JULIUS_11892.py

--- Tugas_Modul4_B_JULIUS_11892.py
+++ Tugas_Modul4_B_JULIUS_11892.py
@@ -47,10 +47,11 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - JULIUS</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = 
-        r'AGG_model.pkl',
-        'KMeans_model.pkl',
-        'DBSCAN_model.pkl'
+    model_path = {
+        "AGG_model" :  r'AGG_model.pkl',
+        "KMeans_model" :  r'KMeans_model.pkl',
+        "DBSCAN_model" :  r'DBSCAN_model.pkl',
+    }
 
     models = {}
 
